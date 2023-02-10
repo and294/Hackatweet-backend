a5873db6952fcb0c052857be533e2dab9c675edc
Sacha <Sacha.bigou@gmail.com>
2023-02-10 16:54:03 +0100

    final countdown

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,50 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-require('../models/connection');
-const Tweet = require('../models/tweets');
+require("../models/connection");
+const Tweet = require("../models/tweets");
 
 // GET all tweets
 
-router.get('/', function(req, res) {
-    Tweet.find()
-    .then(data => {
-        if (data) {
-            res.json({tweet: data});
-        } else {
-            res.json({message: 'Twitter is down'});
-        }
-});
+router.get("/", function (req, res) {
+  Tweet.find().then((data) => {
+    if (data) {
+      res.json({ tweet: data });
+    } else {
+      res.json({ message: "Twitter is down" });
+    }
+  });
 });
 
 // POST a tweet
 
-router.post('/add', function(req, res) {
-    const { firstname, username, content } = req.body;
+router.post("/add", function (req, res) {
+  const { firstname, username, content } = req.body;
 
-    console.log(req.body);
-    const tweet = new Tweet({ firstname, username, content });
-    tweet.save()
-    .then(tweet => {
-        res.json({tweet: tweet.content});
+  console.log(req.body);
+  const tweet = new Tweet({ firstname, username, content });
+  tweet
+    .save()
+    .then((tweet) => {
+      res.json({ tweet: tweet.content });
     })
-    .catch(err => {
-        res.json({message: 'Twitter is down'});
+    .catch((err) => {
+      res.json({ message: "Twitter is down" });
     });
 });
 
 // DELETE a tweet
 
-router.delete('/del/:id', function(req, res) {
-    const { id } = req.params;
-    Tweet.deleteOne({_id: id})
-    .then(tweet => {
-        res.json(tweet);
+router.delete("/del/:id", function (req, res) {
+  const { id } = req.params;
+  Tweet.deleteOne({ _id: id })
+    .then((tweet) => {
+      res.json(tweet);
     })
-    .catch(err => {
-        res.json({message: 'Twitter is down'});
+    .catch((err) => {
+      res.json({ message: "Twitter is down" });
     });
 });
-
 
 module.exports = router;
